a1c13954509e6f0175880a6e24d14c8c4b7831be
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-09-25 20:39:42 +0200

    fetched categories and parsed data from json

--- Netflix-UI-master/Home-Page/scriptNew.js
+++ Netflix-UI-master/Home-Page/scriptNew.js
@@ -1,32 +1,37 @@
 window.onload = async () => {
+  // Variables for Fetch URL & authorization key
   const url = "https://striveschool-api.herokuapp.com/api/movies/";
   const key =
     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGI4YThiODE2ZWY2MDAwMTVjZWQwNTUiLCJpYXQiOjE2MzIzMTU3ODksImV4cCI6MTYzMzUyNTM4OX0.5DMyLDm1BhaVjrBNllHevG_JWKkIIT8o2np1ZpuHMFw";
 
+  // Reference to categories in the DOM
   const drama = document.getElementById("drama");
   const comedy = document.getElementById("comedy");
   const thriller = document.getElementById("thriller");
 
+  // The main Try/Catch
   try {
+    // The response from Drama category
     const dramaResponse = await fetch(url + "Drama", {
       method: "GET",
       headers: {
         Authorization: key,
       },
     });
-
+    // The response from Comedy category
     const comedyResponse = await fetch(url + "Comedy", {
       method: "GET",
       headers: {
         Authorization: key,
       },
     });
-
+    // The response for Thriller category
     const thrillerResponse = await fetch(url + "Thriller", {
       headers: "GET",
       Authorization: key,
     });
 
+    // The parsed data from each category
     const dramaData = await dramaResponse.json();
     const comedyData = await comedyResponse.json();
     const thrillerData = await thrillerResponse.json();
